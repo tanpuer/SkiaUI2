5de53e7720ff6e4baff7f2817486508beedeff72
chenwei <cw404021@alibaba-inc.com>
2024-02-12 15:49:46 +0800

    fullscreen

--- app/src/main/java/com/temple/skiaui/MainActivity.kt
+++ app/src/main/java/com/temple/skiaui/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,14 +13,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         addSkiaView()
+        val controller = WindowCompat.getInsetsController(window, window.decorView)
+        controller.hide(WindowInsetsCompat.Type.statusBars());
     }
 
     private fun addSkiaView() {
+        val layoutParams = ConstraintLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
         findViewById<ConstraintLayout>(R.id.clContainer).addView(
             HYSkiaSurfaceView(this),
 //            HYSkiaTextureView(this),
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.MATCH_PARENT
+            layoutParams
         )
     }
 
