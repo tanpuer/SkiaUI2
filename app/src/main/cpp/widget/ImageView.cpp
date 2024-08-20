68e316db1b116bbc0bfb76431327b6444cc6e8ce
chenwei <templechen@163.com>
2024-08-20 22:13:26 +0800

    fix rotate

--- app/src/main/cpp/widget/ImageView.cpp
+++ app/src/main/cpp/widget/ImageView.cpp
@@ -92,7 +92,7 @@ void ImageView::layout(int l, int t, int r, int b) {
                                  dstRect.centerY());
         }
     }
-    imageMatrix.setRotate(rotateZ, dstRect.centerX(), dstRect.centerY());
+    imageMatrix.preRotate(rotateZ, dstRect.centerX(), dstRect.centerY());
 }
 
 void ImageView::draw(SkCanvas *canvas) {
