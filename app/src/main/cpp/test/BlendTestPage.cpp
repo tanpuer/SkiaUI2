659439838a57e3ef24aea8aac5b9c7df35d30d21
chenwei <cw404021@alibaba-inc.com>
2025-04-15 11:12:25 +0800

    update blend demo

--- app/src/main/cpp/test/BlendTestPage.cpp
+++ app/src/main/cpp/test/BlendTestPage.cpp
@@ -2,6 +2,7 @@
 #include "ScrollView.h"
 #include "BlendTestView.h"
 #include "TextView.h"
+#include "ImageView.h"
 
 namespace HYSkiaUI {
 
@@ -27,6 +28,59 @@ void BlendTestPage::initChildren(ViewGroup *root, int width, int height) {
     scrollView->setAlignItems(YGAlignCenter);
     scrollView->setFlex(1);
     this->addView(scrollView);
+
+    {
+        auto flexboxLayout = new BlendTestPage::LightenFlexboxLayout();
+        flexboxLayout->setContext(this->context);
+        flexboxLayout->setWidth(width);
+        flexboxLayout->setStyle(SkPaint::kFill_Style);
+        flexboxLayout->setBackgroundColor(SK_ColorGRAY);
+        flexboxLayout->setStrokeWidth(0);
+        flexboxLayout->setMargin({0, 0, 0, 50});
+        flexboxLayout->setFlexDirection(YGFlexDirection::YGFlexDirectionRow);
+        flexboxLayout->setJustifyContent(YGJustify::YGJustifyFlexStart);
+        flexboxLayout->setAlignItems(YGAlign::YGAlignCenter);
+        scrollView->addView(flexboxLayout);
+
+        {
+            auto imageView = new ImageView();
+            imageView->setContext(this->context);
+            imageView->setSource("bird.gif");
+            imageView->setScaleType(ImageView::ScaleType::FitCenter);
+            imageView->setStyle(SkPaint::kStroke_Style);
+            imageView->setBackgroundColor(SK_ColorTRANSPARENT);
+            imageView->setStrokeWidth(0);
+            imageView->setWidth(400);
+            imageView->setHeight(250);
+            imageView->setScaleEffect(true);
+            flexboxLayout->addView(imageView);
+        }
+
+        {
+            auto imageView = new ImageView();
+            imageView->setContext(this->context);
+            imageView->setWidth(170 * 2);
+            imageView->setHeight(46 * 2);
+            imageView->setSource("music/logo.png");
+            imageView->setScaleType(ImageView::ScaleType::CenterCrop);
+            imageView->setStyle(SkPaint::kStroke_Style);
+            imageView->setBackgroundColor(SK_ColorTRANSPARENT);
+            imageView->setStrokeWidth(0);
+            flexboxLayout->addView(imageView);
+        }
+
+        {
+            auto textView = new TextView();
+            textView->setContext(this->context);
+            textView->setText("羊肉烤串");
+            textView->setTextColor(SK_ColorGREEN);
+            textView->setTextSize(50);
+            textView->setBackgroundColor(SK_ColorTRANSPARENT);
+            textView->setStyle(SkPaint::kStroke_Style);
+            textView->setMargin({50, 50, 50, 50});
+            flexboxLayout->addView(textView);
+        }
+    }
 
     static std::unordered_map<std::string, SkBlendMode> blendMap = {
             {"kSrc",        SkBlendMode::kSrc},
--- app/src/main/cpp/test/BlendTestPage.h
+++ app/src/main/cpp/test/BlendTestPage.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "Page.h"
+#include "FlexboxLayout.h"
+#include "effects/SkGradientShader.h"
+#include "LinearAnimator.h"
 
 namespace HYSkiaUI {
 
@@ -13,6 +16,57 @@ public:
 private:
 
     void initChildren(ViewGroup *root, int width, int height);
+
+public:
+
+    class LightenFlexboxLayout : public FlexboxLayout {
+
+    public:
+
+        void layout(int l, int t, int r, int b) override {
+            FlexboxLayout::layout(l, t, r, b);
+            if (firstFlag) {
+                delta = -width;
+                firstFlag = false;
+                auto animator = new LinearAnimator(this, -width, width);
+                animator->setEaseType(EaseType::Linear);
+                animator->setLoopCount(-1);
+                animator->setDuration(5000);
+                animator->setUpdateListener([this](View *view, float value) {
+                    this->delta = value;
+                });
+                animator->start();
+            }
+        }
+
+        void draw(SkCanvas *canvas) override {
+            FlexboxLayout::draw(canvas);
+            SkColor colors[] = {
+                    SK_ColorTRANSPARENT,
+                    SK_ColorMAGENTA,
+                    SK_ColorMAGENTA,
+                    SK_ColorTRANSPARENT,
+            };
+            SkPoint horz[] = {
+                    {left + this->delta,  static_cast<float >(top)},
+                    {right + this->delta, static_cast<float >(top)},
+            };
+            SkPaint paint;
+            paint.setShader(SkGradientShader::MakeLinear(horz, colors, nullptr, std::size(colors),
+                                                         SkTileMode::kClamp));
+            canvas->save();
+            paint.setBlendMode(SkBlendMode::kPlus);
+            canvas->drawPaint(paint);
+            canvas->restore();
+        }
+
+    private:
+
+        bool firstFlag = true;
+
+        float delta = 0.0f;
+
+    };
 
 };
 
